9b2ba5ab040e447ece33e8b85ff6292fc58a4ad7
Toludoyin <toludoyinshopein@gmail.com>
2022-09-01 16:39:23 +0100

    danny's dinner request done

--- 8weeks-sql-challenge/dannys-dinner.sql
+++ 8weeks-sql-challenge/dannys-dinner.sql
@@ -12,11 +12,8 @@ CASE STUDY QUESTIONS
 8. What is the total items and amount spent for each member before they became a member?
 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
-
-BONUS QUESTIONS
-1. join all the things
-2. rank all the things
 */
+
 
 -- ANSWERS TO QUESTIONS
 select
@@ -121,12 +118,50 @@ where me.join_date > s.order_date
 group by 1
 order by 1
 
+
 -- QUESTION 9
+with multiplier as (
+    select
+    s.customer_id,
+    m.product_name,
+    m.price,
+    case when product_id = 1 then m.price*20 else m.price*10
+    end as points
+    from dannys_diner.menu m
+    join dannys_diner.sales s using(product_id)
+)
+select
+customer_id,
+sum(points) as total_price
+from multiplier
+group by 1
+order by 2 desc
 
 
 -- QUESTION 10
-
-
+with earnings as (
+    select
+    customer_id,
+    s.product_id,
+    date_trunc('week', s.order_date) as order_week,
+    date_trunc('week', m.join_date) as join_week
+    from dannys_diner.sales s
+    join dannys_diner.members m using(customer_id)
+    where m.join_date <= s.order_date
+)
+select
+customer_id,
+sum(total_point) as total_point
+from (
+    select
+    ea.*,
+    price,
+    (price * 20) as total_point
+    from earnings ea
+    join dannys_diner.menu me using(product_id)
+    where extract(month from order_week) = 01
+) as stop
+group by 1
 
 
 
