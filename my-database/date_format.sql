c0bd19ba43c1bf0f71ccad25683f3ed9a6ce6971
Toludoyin <toludoyinshopein@gmail.com>
2023-01-27 23:40:44 +0100

    add generate_series

--- my-database/date_format.sql
+++ my-database/date_format.sql
@@ -1,3 +1,11 @@
+-- date_trunc()
 select
 date_trunc('day', rental_date) as r_date
-from rentals
+from rentals;
+
+-- generate series
+select generate_series(start, end, step);
+
+-- Create lower and upper bounds of bins
+select generate_series(2200, 3100, 50) as lower,
+       generate_series(2205, 3105, 50) as upper;
