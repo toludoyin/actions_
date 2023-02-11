2ae13efca5b9c5972b8da4a0108bca82b4ed01c9
Toludoyin <toludoyinshopein@gmail.com>
2023-02-11 20:39:45 +0100

    add statistics

---
+++ statistics/stat-101.sql
@@ -0,0 +1,31 @@
+/**
+STATISTICS
+branch of statistics
+* descriptive/ summary statistics: describes data
+* inferential: using samole data to represent population
+
+type of data
+* categorical data: norminal(describes unordered category) & ordinal
+* numerical data: continous & interval(count)
+
+measures of center
+* mean, median, mode
+
+measures of spread
+* range: max-min
+* variance
+* standard deviation
+* quartile
+* interquartile-range
+
+PROBABILITY
+## ways event will happen/ total possible outcome
+* conditional prob: dependent & independent
+
+* discrete & continous probability
+
+* binomial distribution
+
+* normal distribution: skweness, kurtosis
+**/
+
