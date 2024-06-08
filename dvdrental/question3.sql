57c4141ca85c1a615b323f198391e0c2e319dd1f
Toludoyin Shopein <toludoyinshopein@gmail.com>
2024-06-08 05:33:30 +0100

    change altered column name to original name

--- dvdrental/question3.sql
+++ dvdrental/question3.sql
@@ -1,16 +1,16 @@
 /*
 3.
-Show the list of 10 most profitable staff (according to the average lifetime profitabiity i.e. how much they've made per day since employment) [hint: use the last_update field on staff table]
+Show the list of 10 most profitable staff (according to the average lifetime profitabiity i.e.
+how much they've made per day since employment) [hint: use the last_update field on staff table]
 */
 
 select
 staff_id,
 initcap(ss.first_name||' '||ss.last_name) as staff_first_last_name,
 ss.last_update,
-count(dat) as number_of_days,
+count(last_update) as number_of_days,
 sum(amount) as amount,
-round(sum(amount)/count(dat),2) as profit
+round(sum(amount)/count(last_update),2) as profit
 from staff ss
 join payment using(staff_id)
 group by 1,2,3
-
