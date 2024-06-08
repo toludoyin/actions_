a33b2f03459c8a6aea9d3b345c2957abc304201d
Toludoyin Shopein <toludoyinshopein@gmail.com>
2024-06-08 05:37:52 +0100

    replace altered column with original column name

--- dvdrental/question4.sql
+++ dvdrental/question4.sql
@@ -5,27 +5,27 @@ Show daily, weekly and monthly sales with a periodic total and cumulative total 
 
 --daily
 select
-date_trunc('day', dat) as daily,
+date_trunc('day', payment_date) as daily,
 sum(amount) as periodic_total,
-sum(sum(amount)) over(order by date_trunc('day', dat) rows unbounded preceding) as cummulative_total
+sum(sum(amount)) over(order by date_trunc('day', payment_date) rows unbounded preceding) as cummulative_total
 from payment
 group by 1
 order by 1
 
 --weekly
 select
-date_trunc('week', dat) as daily,
+date_trunc('week', payment_date) as daily,
 sum(amount) as periodic_total,
-sum(sum(amount)) over(order by date_trunc('week', dat) rows unbounded preceding) as cummulative_total
+sum(sum(amount)) over(order by date_trunc('week', payment_date) rows unbounded preceding) as cummulative_total
 from payment
 group by 1
 order by 1
 
 --monthly
 select
-date_trunc('month', dat) as daily,
+date_trunc('month', payment_date) as daily,
 sum(amount) as periodic_total,
-sum(sum(amount)) over(order by date_trunc('month', dat) rows unbounded preceding) as cummulative_total
+sum(sum(amount)) over(order by date_trunc('month', payment_date) rows unbounded preceding) as cummulative_total
 from payment
 group by 1
 order by 1
